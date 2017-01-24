05c543213d2fab268899d2f07ba7a03d23a13d7e
Kiki-Jiang <xij035@ucsd.edu>
2017-01-23 20:33:17 -0800

    setup

---
+++ top_movie_viewer/top_movie_viewer/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  top_movie_viewer
+//
+//  Created by 蒋逍琦 on 1/23/17.
+//  Copyright © 2017 蒋逍琦. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
