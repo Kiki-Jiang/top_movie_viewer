70bbddcbc5ae42e21cf8ad0959a856e569930ab7
Kiki-Jiang <xij035@ucsd.edu>
2017-02-05 18:06:35 -0800

    roughly done

---
+++ top_movie_viewer/Pods/Target Support Files/Pods-top_movie_viewer/Pods-top_movie_viewer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_top_movie_viewer : NSObject
+@end
+@implementation PodsDummy_Pods_top_movie_viewer
+@end
---
+++ top_movie_viewer/Pods/Target Support Files/Pods-top_movie_viewerUITests/Pods-top_movie_viewerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_top_movie_viewerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_top_movie_viewerUITests
+@end
